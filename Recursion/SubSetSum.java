ce02c378cb270baa9ecee9857d09e01cb5798cce
sreeraj s <sreerajavk123@gmail.com>
2022-02-24 17:46:07 +0530

    print all unique subset code is added

--- Recursion/SubSetSum.java
+++ Recursion/SubSetSum.java
@@ -4,6 +4,10 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import javax.lang.model.element.Element;
+
+import org.omg.PortableInterceptor.INACTIVE;
+
 /*
     program to print all possible subset sum in increasing order
 */
@@ -11,19 +15,30 @@ public class SubSetSum {
     
     public static void main(String[] args) {
         
-        int[] arr = {3,1,2};
+        int[] arr = {1,1,2,3};
         List<Integer> sumList = new ArrayList<>();
+
+        
         findAllSubsetSum(0, arr, 0, sumList);
         Collections.sort(sumList);
         for(Integer i : sumList) {
             System.err.print(i + " ");
         }
 
+
         findAllSubsets(0, arr, new ArrayList<>());
+
+
+        List<List<Integer>> output = new ArrayList<>(); 
+        printUniqueSubsets(0, arr, new ArrayList<>(), output);
+        for(List<Integer> list : output) {
+            System.err.println(list.toString());
+        }
+
     }
 
 
-    // Functoin to print all subset sums
+    // Function to print all subset sums
     static void  findAllSubsetSum(int i , int[] arr , int sum, List<Integer> sumList) {
 
         if( i >= arr.length ) {
@@ -58,5 +73,19 @@ public class SubSetSum {
         list.remove(list.size() - 1);
         findAllSubsets(i+1, arr, list);
         
+    }
+
+
+    //Function to print all unique subsets in an array ( eliminate duplicates )
+    static void printUniqueSubsets(int i , int[] arr , List<Integer> list, List<List<Integer>> output)  {
+
+        output.add(new ArrayList<>(list));
+
+        for(int j=i; j < arr.length; j++) {
+            if(j > i && arr[j] == arr[j-1]) continue;
+            list.add(arr[j]);
+            printUniqueSubsets(j+1, arr, list, output);
+            list.remove(list.size() - 1);
+        }
     }
 }
