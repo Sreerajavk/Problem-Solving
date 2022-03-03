ddce502eeb877358ed13cfa783bdba75de64c75a
sreeraj s <sreerajavk123@gmail.com>
2022-03-03 16:39:50 +0530

    N queens problem added

--- Recursion/NQueens.java
+++ Recursion/NQueens.java
@@ -13,31 +13,32 @@ public class NQueens {
         int n = 4;
         //findDiagonals(n, 2, 2);
 
-        findNQueensPositions(n, new ArrayList<>(),0,0);
+        findNQueensPositionsWithList(n, new ArrayList<>(),0,0);
+        findNQueensPositionsWithArray(n, new  int[n][n], 0, 0);
     }
 
 
-    static void findNQueensPositions(int n, List<List<Integer>> list,int row , int col) {
-         
+    static void findNQueensPositionsWithList(int n, List<List<Integer>> list,int row , int col) {
+
         if(col >= n) {
             System.err.println(list.toString());
             return;
         }
 
         for(int i=row; i<n; i++) {
-            if(isPlacable(n, list, i, col)) {
+            if(isPlacableWithList(n, list, i, col)) {
                 List<Integer> indices = new ArrayList<>();
                 indices.add(i);
                 indices.add(col);
                 list.add(indices);
-                findNQueensPositions(n, list, 0, col+1);
+                findNQueensPositionsWithList(n, list, 0, col+1);
                 list.remove(list.size()-1);
             }
         }
     }
 
     // checks whether a queen can be placed at index (row,col)
-    static Boolean isPlacable(int n , List<List<Integer>> list , int row , int col) {
+    static Boolean isPlacableWithList(int n , List<List<Integer>> list , int row , int col) {
         int i,j;
         for(List<Integer> index : list) {
             i = index.get(0);
@@ -49,6 +50,44 @@ public class NQueens {
 
         return true;
     }
+
+
+    static void findNQueensPositionsWithArray(int n, int[][] indexArray,int row , int col) {
+
+        if(col >= n ) {
+            for(int i=0; i<n; i++) {
+                for(int j=0; j<n; j++) {
+                    System.out.print(indexArray[i][j]);
+                }
+                System.err.println();
+            }
+            System.err.println();
+            return;
+        }
+
+        for(int i=row; i<n; i++) {
+            if(isPlacableWithArray(n, indexArray, i, col)) {
+                indexArray[i][col] = 1;
+                findNQueensPositionsWithArray(n, indexArray, 0, col+1);
+                indexArray[i][col] = 0;
+            }
+        }
+
+    }
+
+    static Boolean isPlacableWithArray(int n , int[][] indexArray , int row , int col) {
+        
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<n; j++) {
+                if(indexArray[i][j] == 1) {
+                    if( i == row || j == col) return false;
+                    if(Math.abs(row-i) == Math.abs(col-j)) return false;
+                }
+            }
+        }
+        return true;
+    }
+
 
 
     // To find all the diagonals of a given index
